8dfe744148f6d2dd8573fa4af95ee85007df66f4
jeremiah.1582@googlemail.com <jeremiah.1582@googlemail.com>
2023-01-20 19:26:34 +0100

    lab 130 complete

---
+++ lab_129_debugger/main.py
@@ -0,0 +1,15 @@
+# you will use the pdb to step through the scripts you wrote in previous labs.
+
+# In this lab, you will:
+    # Explore the basic features of the Python Debugger
+    # Use the Python Debugger to step through Python scripts
+
+# Dynamic DEBUGGING command tool: write NEXT to move to the next line.
+    #  python -m pdb <Name_of_file-to_debug >
+    #>> next 
+#
+
+name = "John"
+print("Hello " + name + ".")
+age = 40
+print(name + " is " + str(age) + " years old.")
